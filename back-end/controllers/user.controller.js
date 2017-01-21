d5140b4c5a507a9dc493eaa598f5738a272f94fb
Aliaksandr <a.tolkach@aisnovations.com>
2017-01-21 17:19:14 +0300

    haven't realised callbacks

---
+++ back-end/controllers/user.controller.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+
+
+module.exports.getUsersList = (req, res) => {}
+
+module.exports.getUserInfo = (req, res) => {}
+
+module.exports.setUserVisibility = (req, res) => {}
