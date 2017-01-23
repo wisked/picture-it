634cdb808954a42fa39b03fd69ff560b562fc736
Aliaksandr <a.tolkach@aisnovations.com>
2017-01-23 18:33:52 +0300

    fixed :

--- back-end/models/Image.js
+++ back-end/models/Image.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const ImageSchema = new Schema({
     _userId : [{ type: Schema.Types.ObjectId, ref: 'User'}],
     url     : {type: String},
-    likes   : [Schema.Types.ObjectId})]
+    likes   : [Schema.Types.ObjectId]
 })
 
-const Image = mongoose.model('Image': ImageSchema);
+mongoose.model('Image', ImageSchema);
