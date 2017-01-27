5444e8cf7cd5380c5b789211aaa4d2a71d50543c
Aliaksandr <a.tolkach@aisnovations.com>
2017-01-27 17:58:23 +0300

    created functions login, register

--- front-end/src/modules/auth.js
+++ front-end/src/modules/auth.js
@@ -1,6 +1,21 @@
 import angular from 'angular';
 
-angular.module('app.auth', [])
 
-.controller('authCtrl', function($scope) {
+import '../modules/auth';
+
+
+angular.module('app.auth', [
+    'auth.service'
+])
+
+.controller('authCtrl', function($scope, AuthService, $state) {
+    $scope.login = function () {
+        AuthService.login($scope.username, $scope.password)
+        $state.transitionTo('home')
+    }
+
+    $scope.register = function () {
+        AuthService.register($scope.email, $scope.username, $scope.password)
+        $state.transitionTo("home")
+    }
 })
