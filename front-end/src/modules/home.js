e2f331e4ecff57511b559e6e6457428ea60f427c
Aliaksandr <a.tolkach@aisnovations.com>
2017-01-27 17:58:35 +0300

    created homeCtrl

---
+++ front-end/src/modules/home.js
@@ -0,0 +1,11 @@
+import angular from 'angular';
+import  angularUiBootstrap from 'angular-ui-bootstrap';
+
+
+angular.module('app.home', [
+    angularUiBootstrap
+])
+.controller('homeCtrl', function($scope, $rootScope) {
+    
+
+})
