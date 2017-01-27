aa1cfbf5b9d254e08595da12c3c8c456e625be0f
Aliaksandr <a.tolkach@aisnovations.com>
2017-01-27 18:00:23 +0300

    added dependency injection auth.service, app.home, app.auth

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -1,9 +1,15 @@
 import angular from 'angular';
 
+import './services/authService'
+
 import './modules/auth';
+import './modules/home';
+
 import './app.config';
 
 angular.module('imgStore', [
     'app.config',
-    'app.auth'
+    'app.auth',
+    'app.home',
+    'auth.service'
 ])
