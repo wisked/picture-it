90e90b5a12986a18dc294c5a3d6b560557ac9e68
Aliaksandr <a.tolkach@aisnovations.com>
2017-01-30 18:28:47 +0300

    added run

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -1,9 +1,11 @@
 import angular from 'angular';
+import uiRouter from 'angular-ui-router';
 
-import './services/authService'
 
 import './modules/auth';
 import './modules/home';
+
+import './services/authService'
 
 import './app.config';
 
@@ -11,5 +13,31 @@ angular.module('imgStore', [
     'app.config',
     'app.auth',
     'app.home',
-    'auth.service'
+    'auth.service',
+    uiRouter
 ])
+
+.run(['$rootScope', '$state', '$stateParams', 'AuthService',
+    function ($rootScope, $state, $stateParams, AuthService) {
+        $rootScope.$on("$stateChangeStart", async function (event, toState, toParams, fromState, fromParams) {
+            if (toState.authenticate && !AuthService.isAuthenticated()) {
+                // User isn’t authenticated
+                event.preventDefault();
+                $state.transitionTo("login");
+            }
+        });
+    }
+])
+
+.controller('appCtrl', function($scope, $state, $rootScope, AuthService) {
+    $scope.authenticated = AuthService.isAuthenticated();
+    // $scope.logout = function () {
+    //     AuthService.logout()
+    //     $scope.authenticated = false
+    //     $state.transitionTo('login')
+    // }
+
+    $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+        $scope.authenticated = AuthService.isAuthenticated();
+    });
+})
