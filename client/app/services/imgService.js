2eaf31c089e1b4ef9cd9bcac3c746606e41b5d52
Aliaksandr <a.tolkach@aisnovations.com>
2017-01-31 18:05:01 +0300

    added deleteUserImg function

--- client/app/services/imgService.js
+++ client/app/services/imgService.js
@@ -2,13 +2,13 @@ angular.module('img.service', [])
 
 .factory('ImageService', function($http) {
     const img = {};
-
+    
     img.delete = function(url) {
         return $http.post('/delete', {
             url: url
         });
     }
-    img.deleteUserImg = function (url, user) { 
+    img.deleteUserImg = function (url, user) {
         return $http.post('/deleteUserImg', {
             url: url,
             user: user
