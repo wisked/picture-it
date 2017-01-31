a5766a6a6b585cf3f6afce9a97df7e3107060ec7
Aliaksandr <a.tolkach@aisnovations.com>
2017-01-31 17:58:37 +0300

    added DI ng-file-upload, realised loadImgs

--- front-end/src/services/imgService.js
+++ front-end/src/services/imgService.js
@@ -1,10 +1,25 @@
 import angular from 'angular'
+import ngFileUpload from 'ng-file-upload';
 
-angular.module('img.service', [])
 
-.factory('ImageService', function($http) {
+angular.module('img.service', [
+    ngFileUpload
+])
+
+.factory('ImageService', function($http, Upload) {
     const img = {};
 
+    img.loadImgs = function (files) {
+        Upload.upload({
+            url: '/api/loadImgs',
+            data: {file: files}
+        })
+        .then(res => {
+            if (res.status = 200) {
+                return res.data;
+            }
+        })
+    }
     img.delete = function(url) {
         return $http.post('/api/delete', {
             url: url
