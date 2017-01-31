344a839a10315345d92e080e01154b6c9e2e9cb4
Aliaksandr <a.tolkach@aisnovations.com>
2017-01-31 17:55:28 +0300

    fixed post on get, added route loadImgs

--- back-end/routes/image.routes.js
+++ back-end/routes/image.routes.js
@@ -2,8 +2,9 @@ const router = require('express').Router();
 const imgCtrl = require('../controllers/images.controller');
 
 
-router.post('/images', imgCtrl.getUserImages)
+router.get('/images', imgCtrl.getUserImages)
 router.post('/delete-img', imgCtrl.deleteUserImage)
+router.post('/loadImgs', imgCtrl.uploadImage)
 
 
 module.exports = router;
