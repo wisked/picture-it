76fcc96a82ae1eeea36daf69c5e1317ae086c47c
Aliaksandr <a.tolkach@aisnovations.com>
2017-01-31 18:06:13 +0300

    added uploadImage controller

--- back-end/controllers/images.controller.js
+++ back-end/controllers/images.controller.js
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose';
+import cloudinary from 'cloudinary';
+
+import configs from '../../etc/config.json'
 
 const Image = mongoose.model('Image');
 
+const clodinaryConfigs = configs.cloudinary;
+
+cloudinary.config(clodinaryConfigs)
 
 export const getUserImages = (req, res) => {
     Image.find({
@@ -39,6 +45,25 @@ export const deleteUserImage = (req, res) => {
         })
 }
 
+export const uploadImage = (req, res, next) => {
+        if (req.files.image) {
+            cloudinary.uploader.upload(req.files.image.path, function (result) {
+                if (result.url) {
+                    let image = new Image();
+                    image.url = result.url;
+                    image._owner = req.params.id ? req.params.id : req.session._id;
+                    image.save((err, response) => {
+                        res.status(201).json(result.url)
+                    })
+                } else {
+                    res.json(error);
+                }
+            });
+        }
+        else {
+            next();
+        }
+    }
 export const getImageInfo = (req, res) => {
 
 }
