3d63e16bf1136e7d8077c08f195d48b39b94ca27
Aliaksandr <a.tolkach@aisnovations.com>
2017-01-31 17:57:21 +0300

    deleted async, added checkProfileVisability function

--- front-end/src/services/userService.js
+++ front-end/src/services/userService.js
@@ -5,16 +5,17 @@ angular.module('user.service', [])
 .factory('UserSevice', function($http) {
     const user = {};
 
-    user.updateProfile = async function(profile) {
-        let res = await $http.post('/api/update', {
-            profile: profile
-        })
+    user.updateProfile = function(profile) {
+        return $http.post('/api/set-userVisibility', {profile: profile})
+            .then(response => response.data)
     }
     user.getUserList = function () {
         return $http.get('/api/get-usersList')
-        .then(res => res.data)
-
-
+                .then(res => res.data)
+    }
+    user.checkProfileVisability = function () {
+        return $http.get('/api/check-userProfile')
+                .then(res => res.data)
     }
     return user
 })
