4ec9a958e7a01c5e0b0b2ad318e55910751260d5
Aliaksandr <a.tolkach@aisnovations.com>
2017-02-01 18:01:50 +0300

    realised ckeckProfile for user

--- front-end/src/modules/user.js
+++ front-end/src/modules/user.js
@@ -1,7 +1,27 @@
 import angular from 'angular'
+import angularUiBootstrap from 'angular-ui-bootstrap';
+import ngFileUpload from 'ng-file-upload';
+import uiRouter from 'angular-ui-router';
 
+import '../services/userService';
+import '../services/imgService'
 
-angular.module('app.user', [])
-.controller('userCtrl', function ($scope) {
-    
+angular.module('app.user', [
+    'user.service',
+    'img.service',
+    uiRouter,
+    ngFileUpload,
+    angularUiBootstrap
+])
+.controller('userCtrl', function ($scope, $rootScope, UserSevice, $stateParams) {
+    $scope.header = "Edit profile"
+    $scope.isAdmin = $rootScope.user.isAdmin
+
+    let profilePromise = UserSevice.checkProfileVisability($stateParams.id)
+    profilePromise.then(res => $scope.profileVisability = res.profile)
+
+    $scope.changeProfile = function (checkBoxValue) {
+        let profilePromise = UserSevice.updateProfile(checkBoxValue)
+        profilePromise.then(res => $scope.profileVisability)
+    }
 })
