ed28fe432a1a2d178faef30c457826703ac13000
Aliaksandr <a.tolkach@aisnovations.com>
2017-02-01 17:59:54 +0300

    added multiparty middleware

--- back-end/routes/image.routes.js
+++ back-end/routes/image.routes.js
@@ -1,10 +1,14 @@
+import multiparty from 'connect-multiparty';
+const multipartyMiddleware = multiparty()
+
 const router = require('express').Router();
 const imgCtrl = require('../controllers/images.controller');
 
 
 router.get('/images', imgCtrl.getUserImages)
 router.post('/delete-img', imgCtrl.deleteUserImage)
-router.post('/loadImgs', imgCtrl.uploadImage)
+router.post('/loadImgs', multipartyMiddleware, imgCtrl.uploadImage)
+router.post('/loadImgs/', multipartyMiddleware, imgCtrl.uploadImage)
 
 
 module.exports = router;
