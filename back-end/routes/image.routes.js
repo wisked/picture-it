d312264e55320b3e70004dfcd91e92e6846fd118
Aliaksandr <koder@gmail.com>
2017-02-01 22:46:53 +0300

    deleteed dublicated route

--- back-end/routes/image.routes.js
+++ back-end/routes/image.routes.js
@@ -8,7 +8,6 @@ const imgCtrl = require('../controllers/images.controller');
 router.get('/images', imgCtrl.getUserImages)
 router.post('/delete-img', imgCtrl.deleteUserImage)
 router.post('/loadImgs', multipartyMiddleware, imgCtrl.uploadImage)
-router.post('/loadImgs/', multipartyMiddleware, imgCtrl.uploadImage)
 
 
 module.exports = router;
