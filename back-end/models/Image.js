897259e2035d540f163d1e2df9824b75d79d1ea6
Aliaksandr <a.tolkach@aisnovations.com>
2017-02-02 12:13:50 +0300

    fixed _userId field

--- back-end/models/Image.js
+++ back-end/models/Image.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ImageSchema = new Schema({
-    _userId : [{ type: Schema.Types.ObjectId, ref: 'User'}],
+    _userId : { type: Schema.Types.ObjectId, ref: 'User'},
     likes   : [Schema.Types.ObjectId],
     cloudinary: {
         public_id : {type: String},
