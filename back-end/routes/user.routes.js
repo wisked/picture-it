d06325a25a0ac444d95c750e928e48613fc8e51b
Aliaksandr <a.tolkach@aisnovations.com>
2017-02-02 17:15:35 +0300

    added get-userName route

--- back-end/routes/user.routes.js
+++ back-end/routes/user.routes.js
@@ -5,5 +5,6 @@ const userCtrl = require('../controllers/user.controller');
 router.get('/get-usersList', userCtrl.getUsersList)
 router.post('/set-userVisibility', userCtrl.setUserVisibility)
 router.get('/check-userProfile', userCtrl.checkProfileVisability)
+router.get('/get-userName', userCtrl.getUserName)
 
 module.exports = router;
