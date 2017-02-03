76224b91bede658735e960968ed6b6b399e0e1af
Aliaksandr <a.tolkach@aisnovations.com>
2017-02-03 14:41:52 +0300

    added optional param for loadImgs

--- back-end/routes/image.routes.js
+++ back-end/routes/image.routes.js
@@ -7,7 +7,7 @@ const imgCtrl = require('../controllers/images.controller');
 
 router.get('/getImages/:id?', imgCtrl.getUserImages)
 router.post('/delete-image/:id?', imgCtrl.deleteUserImage)
-router.post('/loadImgs', multipartyMiddleware, imgCtrl.uploadImage)
+router.post('/loadImgs/:id?', multipartyMiddleware, imgCtrl.uploadImage)
 
 
 module.exports = router;
