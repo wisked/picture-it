11adab09219d035b3516cfc9b53d5f000816ff45
Aliaksandr <a.tolkach@aisnovations.com>
2017-02-06 13:28:29 +0300

    added add-like route

--- back-end/routes/image.routes.js
+++ back-end/routes/image.routes.js
@@ -8,6 +8,7 @@ const imgCtrl = require('../controllers/images.controller');
 router.get('/getImages/:id?', imgCtrl.getUserImages)
 router.post('/delete-image/:id?', imgCtrl.deleteUserImage)
 router.post('/loadImgs/:id?', multipartyMiddleware, imgCtrl.uploadImage)
+router.post('/add-like/:id?', imgCtrl.addLike)
 
 
 module.exports = router;
