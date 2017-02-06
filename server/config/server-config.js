ed0d203520002c2a5183e313ff7d10bbe486349b
Aliaksandr <a.tolkach@aisnovations.com>
2017-02-06 15:25:50 +0300

    deleted app_copy.js folders client $ server

--- server/config/server-config.js
+++
@@ -1,7 +0,0 @@
-const config = {};
-
-config.port = 3001;
-
-config.mongoUri = 'mongodb://localhost:27017/users';
-
-module.exports = config;
--- server/models/images.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const imageSchema = new mongoose.Schema({
-    _owner: {type: Schema.Types.ObjectId, ref: 'User'},
-    url: String
-});
-
-mongoose.model('Image', imageSchema);
