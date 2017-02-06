52b6ebcf15accba17961cf17cf0e8a50ffc32ea0
Aliaksandr <a.tolkach@aisnovations.com>
2017-02-06 15:12:53 +0300

    added params to check-userProfile

--- back-end/routes/user.routes.js
+++ back-end/routes/user.routes.js
@@ -4,7 +4,7 @@ const userCtrl = require('../controllers/user.controller');
 
 router.get('/get-usersList', userCtrl.getUsersList)
 router.post('/set-userVisibility', userCtrl.setUserVisibility)
-router.get('/check-userProfile', userCtrl.checkProfileVisability)
+router.get('/check-userProfile/:id?', userCtrl.checkProfileVisability)
 router.get('/get-userName', userCtrl.getUserName)
 
 module.exports = router;
